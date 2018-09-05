8f2f8e10ebb3646a9c3ec06b50fb7a1e775f3cde
Lee Hudson <lee.hudson@diamond.ac.uk>
2018-09-05 11:12:04 +0100

    Starting builder work

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,36 @@
+import subprocess
+from iocbuilder import Device, AutoSubstitution, Xml
+from iocbuilder.modules.asyn import AsynIP
+from iocbuilder.modules.streamDevice import AutoProtocol
+from iocbuilder.arginfo import *
 
+class smargonLib(Device):
+    LibFileList = ['smargon']
+    DbdFileList = ['smargon']
+    AutoInstantiate = True
+
+#class smargonXmlTemplate(Xml):
+#       TemplateFile = "smargonTemplate.xml"
+
+class Smargon(Device, ):
+    #Dependencies = (ADCore,)
+    def __init__(self, PORTNAME = "SSHP", IP_ADDRESS = "", UNAME = "root", PASSWORD = "deltatau", **args):
+
+    # __init__ arguments
+    ArgInfo =
+            makeArgInfo(__init__,
+        P = Simple("Device Prefix", str),
+        PORTNAME = Simple('Portname', str),
+        IP_ADDRESS = Simple('IP Address of power pmac', str),
+        UNAME = Simple('SSH username for power pmac', str),
+        PASSWORD = Simple('SSH password for power pmac', str))
+
+    # Device attributes
+    #LibFileList = ['smargon']
+    #DbdFileList = ['smargon']
+
+    def Initialise(self):
+        print '# Create SSH Port (PortName, IPAddress, Username, Password, Priority, DisableAutoConnect, noProcessEos) ' \
+        print 'drvAsynPowerPMACPortConfigure("%(PORTNAME)s", "%(IP_ADDRESS)s", ' \
+            '%(UNAME)s, %(PASSWORD)s, 0, 0, 0)'  % self.__dict__
 
